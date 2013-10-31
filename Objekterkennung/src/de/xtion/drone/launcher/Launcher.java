a54e35e1fd8a3fe04796f57ec0474df01c7b1a7c
Robin <Robin@Robin-THINK>
2013-10-31 13:55:18 +0100

    Blabla

--- Objekterkennung/src/de/xtion/drone/launcher/Launcher.java
+++ Objekterkennung/src/de/xtion/drone/launcher/Launcher.java
@@ -1,18 +1,78 @@
 package de.xtion.drone.launcher;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.AbstractAction;
+import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
 import de.xtion.drone.ARDroneController;
 import de.xtion.drone.ColorOBJController;
 
 public class Launcher {
+
+	private final ActionReset actionReset = new ActionReset();
+	private final ActionConnect actionConnect = new ActionConnect();
+	private final ActionShowLiveCam actionShowLiveCam = new ActionShowLiveCam();
+
+
+	private final class ActionReset extends AbstractAction {
+		public ActionReset() {
+			putValue(NAME, "Reset Drohne");
+			setEnabled(false);
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			getArDroneController().resetDrone();
+		}
+	}
+
+
+	private final class ActionConnect extends AbstractAction {
+		
+		public ActionConnect() {
+			putValue(NAME, "Verbinde mit der Drohne");
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			boolean connectToDrone = getArDroneController().connectToDrone();
+			
+			if(connectToDrone){
+				enableActions(true);
+			}else{
+				enableActions(false);
+			}
+		}
+	}
+
+
+	private final class ActionShowLiveCam extends AbstractAction {
+		
+		public ActionShowLiveCam() {
+			putValue(NAME, "Zeige Livebild");
+			setEnabled(false);
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			getArDroneController().showLiveCam(getMonitor0());
+		}
+	}
+
 
 	private static final int NUMBER_OF_COLS_MENUE = 1;
 
@@ -27,28 +87,46 @@ public class Launcher {
 	private JButton vidButton;
 	private JFrame jFrame;
 	private JButton colorTrackButton;
+	private JLabel monitor1;
+	public JLabel getMonitor1() {
+		return monitor1;
+	}
+
+
+	public JLabel getMonitor2() {
+		return monitor2;
+	}
+
+
+	public JLabel getMonitor3() {
+		return monitor3;
+	}
+
+
+	public JLabel getMonitor0() {
+		return monitor0;
+	}
+
+	private JLabel monitor2;
+	private JLabel monitor3;
+	private JLabel monitor0;
 	
 	public Launcher() {
 		
 		jFrame = new JFrame();
 		jFrame.setTitle("Launcher XTion Drone");
 		
+		jFrame.setJMenuBar(getMenu());
+		
+		jFrame.setContentPane(getContent());
 		arDroneController = new ARDroneController();
-		
-		jFrame.setLayout(new GridLayout(0, NUMBER_OF_COLS_MENUE));
-		
-		jFrame.add(getButtonConnectDrone());
-		jFrame.add(getButtonShowVideo());
-		jFrame.add(getButtonColoredTrack());
 		
 		jFrame.addWindowListener(new WindowAdapter() {
 			
 			@Override
 			public void windowClosing(WindowEvent e) {
 				jFrame.setVisible(false);
-				
 				getArDroneController().stop();
-				
 				jFrame.dispose();
 				
 				System.exit(0);
@@ -57,22 +135,74 @@ public class Launcher {
 	}
 	
 
-	private JButton getButtonShowVideo() {
-		if(vidButton == null){
-			vidButton = new JButton();
-			vidButton.setText("Zeige Videoaufzeichnung");
-			vidButton.setEnabled(false);
-			vidButton.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					getArDroneController().showLiveCam();
-				}
-			});
-		}
-		return vidButton;
+	private Container getContent() {
+		JPanel jPanel = new JPanel();
+		jPanel.setLayout(new BorderLayout());
+		
+		jPanel.add(getCenterPanel(), BorderLayout.CENTER);
+		jPanel.add(getSettingsPanel(), BorderLayout.WEST);
+		jPanel.add(getInformationPanel(), BorderLayout.SOUTH);
+		return jPanel;
 	}
-	
+
+
+	private Component getInformationPanel() {
+		return new JPanel();
+	}
+
+
+	private Component getSettingsPanel() {
+		return new JPanel();
+	}
+
+
+	private Component getCenterPanel() {
+		JPanel jPanel = new JPanel();
+		jPanel.setLayout(new GridLayout(2, 2));
+		
+		monitor0 = new JLabel(){
+			@Override
+			public void setIcon(Icon icon) {
+				super.setIcon(icon);
+				if(icon == null){
+					setText("Kein Bild auf Monitor 0");
+				}else{
+					setText("");
+				}
+			}
+		};
+		monitor1 = new JLabel();
+		monitor2 = new JLabel();
+		monitor3 = new JLabel();
+		
+		jPanel.add(monitor0);
+		jPanel.add(monitor1);
+		jPanel.add(monitor2);
+		jPanel.add(monitor3);
+		
+		return jPanel;
+	}
+
+
+	private JMenuBar getMenu() {
+		JMenuBar ret = new JMenuBar();
+		
+		JMenu menuDrone = new JMenu();
+		menuDrone.setText("Drohne");
+		ret.add(menuDrone);
+		menuDrone.add(new JMenuItem(actionConnect));
+		menuDrone.add(new JMenuItem(actionReset));
+		
+		JMenu menuAnsicht = new JMenu();
+		menuAnsicht.setText("Ansicht");
+		ret.add(menuAnsicht);
+		
+		menuAnsicht.add(new JMenuItem(actionShowLiveCam));
+		return ret;
+	}
+
+
+
 	private JButton getButtonColoredTrack() {
 		if(colorTrackButton == null){
 			colorTrackButton = new JButton();
@@ -90,35 +220,16 @@ public class Launcher {
 	}
 
 
-	private JButton getButtonConnectDrone() {
-		if(connectButton == null){
-			connectButton = new JButton();
-			connectButton.setText("Verbinde mit der Drohne");
-			connectButton.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					boolean connectToDrone = getArDroneController().connectToDrone();
-					
-					if(connectToDrone){
-						enableButtons(true);
-					}else{
-						enableButtons(false);
-					}
-				}
-			});
-		}
-		return connectButton;
-	}
-
-	private void enableButtons(boolean b) {
-		getButtonShowVideo().setEnabled(b);
+	private void enableActions(boolean b) {
+		actionShowLiveCam.setEnabled(b);
+		actionReset.setEnabled(b);
 		getButtonColoredTrack().setEnabled(b);
 	}
 
 
 	private void start() {
-		jFrame.setSize(400, 600);
 		jFrame.setVisible(true);
+		jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 	}
 
 
