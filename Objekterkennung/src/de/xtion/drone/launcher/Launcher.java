184316db3a83950bba415246f33cb13ceee1f312
Reilif <reilif.rr@gmail.com>
2013-11-07 10:20:18 +0100

    Update Launcher.java

--- Objekterkennung/src/de/xtion/drone/launcher/Launcher.java
+++ Objekterkennung/src/de/xtion/drone/launcher/Launcher.java
@@ -1,12 +1,43 @@
 package de.xtion.drone.launcher;
 
-import de.xtion.drone.ARDroneController;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.util.Map;
+
+import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.border.Border;
+
+import org.opencv.core.Mat;
+
 import de.xtion.drone.WebCamController;
 import de.xtion.drone.gui.ColorAdjustment;
 import de.xtion.drone.gui.EdgeAdjustment;
 import de.xtion.drone.interfaces.DrohnenController;
+import de.xtion.drone.interfaces.NavController;
+import de.xtion.drone.interfaces.OBJController;
+import de.xtion.drone.manipulation.CircleDetection;
 import de.xtion.drone.manipulation.ColorDetection;
 import de.xtion.drone.manipulation.EdgeDetection;
+import de.xtion.drone.model.CircleModel;
+import de.xtion.drone.model.CircleModel.CircleModelEvent;
 import de.xtion.drone.model.ColorModel;
 import de.xtion.drone.model.ColorModel.ColorModelEvents;
 import de.xtion.drone.model.EdgeModel;
@@ -15,29 +46,62 @@ import de.xtion.drone.model.MainModel;
 import de.xtion.drone.model.util.ModelEvent;
 import de.xtion.drone.model.util.ModelEventListener;
 import de.xtion.drone.utils.ImageUtils;
-import org.opencv.core.Mat;
-
-import javax.swing.*;
-import javax.swing.border.Border;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.image.BufferedImage;
 
 public class Launcher {
+
 	static {
 		System.loadLibrary("opencv_java246");
 	}
+	private static final Border MONITOR_BORDER = BorderFactory
+			.createLineBorder(Color.BLUE);
 
-	private static final Border MONITOR_BORDER = BorderFactory.createLineBorder(Color.BLUE);
-
-	private final ActionShowEdgeCam    actionEdge           = new ActionShowEdgeCam();
+	private final ActionShowEdgeCam actionEdge = new ActionShowEdgeCam();
 	private final ActionShowColorTrack actionShowColorTrack = new ActionShowColorTrack();
-	private final ActionReset          actionReset          = new ActionReset();
-	private final ActionConnect        actionConnect        = new ActionConnect();
-	private final ActionWebCam         actionWebCam         = new ActionWebCam();
-	private final ActionShowLiveCam    actionShowLiveCam    = new ActionShowLiveCam();
+	private final ActionShowCircleTrack actionShowCircleTrack = new ActionShowCircleTrack();
+	private final ActionReset actionReset = new ActionReset();
+	private final ActionConnect actionConnect = new ActionConnect();
+	private final ActionShowLiveCam actionShowLiveCam = new ActionShowLiveCam();
+
+	private final class Monitor extends JLabel {
+
+		private int nr;
+
+		public Monitor(int nr) {
+			this.nr = nr;
+			setIcon(null);
+		}
+
+		public void setImage(BufferedImage bim) {
+			if (bim != null) {
+				int height = bim.getHeight();
+				int width = bim.getWidth();
+
+				double div = (double) height / (double) width;
+
+				Image scaled;
+				if (getWidth() * div > getHeight()) {
+					scaled = bim.getScaledInstance(getWidth(),
+							(int) (getWidth() * div), BufferedImage.SCALE_FAST);
+				} else {
+					scaled = bim.getScaledInstance((int) (getHeight() / div),
+							getHeight(), BufferedImage.SCALE_FAST);
+				}
+
+				ImageIcon icon = new ImageIcon(scaled);
+				setIcon(icon);
+			}
+		}
+
+		@Override
+		public void setIcon(Icon icon) {
+			if (icon == null) {
+				setText("Kein Bild auf Monitor " + nr);
+			} else {
+				setText("");
+			}
+			super.setIcon(icon);
+		}
+	}
 
 	private final class ActionShowColorTrack extends AbstractAction {
 		public ActionShowColorTrack() {
@@ -50,15 +114,45 @@ public class Launcher {
 			final ColorModel colorModel = mainModel.getColorModel();
 			ColorDetection colorDetection = new ColorDetection(colorModel);
 			getArDroneController().addOBJController(colorDetection);
-			colorModel.addModelEventListener(ColorModelEvents.COLOR_IMAGE, new ModelEventListener() {
+			colorModel.addModelEventListener(ColorModelEvents.COLOR_IMAGE,
+					new ModelEventListener() {
 
-				@Override
-				public void actionPerformed(ModelEvent event) {
-					Mat colorImage = colorModel.getColorImage();
-					BufferedImage matToBufferedImage = ImageUtils.matToBufferedImage(colorImage);
-					getMonitor1().setIcon(new ImageIcon(matToBufferedImage));
-				}
-			});
+						@Override
+						public void actionPerformed(ModelEvent event) {
+							Mat colorImage = colorModel.getColorImage();
+							BufferedImage matToBufferedImage = ImageUtils
+									.matToBufferedImage(colorImage);
+
+							getMonitor1().setImage(matToBufferedImage);
+						}
+					});
+		}
+	}
+
+	private final class ActionShowCircleTrack extends AbstractAction {
+		public ActionShowCircleTrack() {
+			putValue(NAME, "Zeige Kreisbild");
+			setEnabled(false);
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			final CircleModel circleModel = mainModel.getCircleModel();
+			final CircleDetection circleDetection = new CircleDetection(
+					circleModel, mainModel.getColorModel(),
+					mainModel.getEdgeModel());
+			getArDroneController().addOBJController(circleDetection);
+
+			circleModel.addModelEventListener(CircleModelEvent.CIRCLE_IMG,
+					new ModelEventListener() {
+
+						@Override
+						public void actionPerformed(ModelEvent event) {
+							BufferedImage colorImage = circleModel
+									.getCircleImage();
+							getMonitor3().setImage(colorImage);
+						}
+					});
 		}
 	}
 
@@ -74,23 +168,15 @@ public class Launcher {
 			final EdgeModel edgeModel = mainModel.getEdgeModel();
 			final EdgeDetection edgeDetection = new EdgeDetection(edgeModel);
 			getArDroneController().addOBJController(edgeDetection);
-			//
-			//			mainModel.getColorModel().addModelEventListener(ColorModelEvents.COLOR_IMAGE, new ModelEventListener() {
-			//
-			//				@Override
-			//				public void actionPerformed(ModelEvent event) {
-			//					edgeDetection.processImage(ImageUtils.matToBufferedImage(mainModel.getColorModel().getColorImage()));
-			//				}
-			//			});
+			edgeModel.addModelEventListener(EdgeModelEvent.EDGE_IMG,
+					new ModelEventListener() {
 
-			edgeModel.addModelEventListener(EdgeModelEvent.EDGE_IMG, new ModelEventListener() {
-
-				@Override
-				public void actionPerformed(ModelEvent event) {
-					BufferedImage edgeImage = edgeModel.getEdgeImage();
-					getMonitor2().setIcon(new ImageIcon(edgeImage));
-				}
-			});
+						@Override
+						public void actionPerformed(ModelEvent event) {
+							BufferedImage edgeImage = edgeModel.getEdgeImage();
+							getMonitor2().setImage(edgeImage);
+						}
+					});
 		}
 	}
 
@@ -107,17 +193,16 @@ public class Launcher {
 	}
 
 	private final class ActionConnect extends AbstractAction {
+
 		public ActionConnect() {
 			putValue(NAME, "Verbinde mit der Drohne");
 		}
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			arDroneController = new ARDroneController();
-
 			boolean connectToDrone = getArDroneController().connectToDrone();
 
-			if(connectToDrone) {
+			if (connectToDrone) {
 				enableActions(true);
 			} else {
 				enableActions(false);
@@ -125,18 +210,8 @@ public class Launcher {
 		}
 	}
 
-	private final class ActionWebCam extends AbstractAction {
-		private ActionWebCam() {
-			putValue(NAME, "Verbinde mit Webcam");
-		}
-
-		@Override public void actionPerformed(ActionEvent e) {
-			arDroneController = new WebCamController();
-			enableActions(true);
-		}
-	}
-
 	private final class ActionShowLiveCam extends AbstractAction {
+
 		public ActionShowLiveCam() {
 			putValue(NAME, "Zeige Livebild");
 			setEnabled(false);
@@ -144,7 +219,17 @@ public class Launcher {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			getArDroneController().showLiveCam(getMonitor0());
+			getArDroneController().addOBJController(new OBJController() {
+				
+				@Override
+				public void processImage(BufferedImage data) {
+					getMonitor0().setImage(data);
+				}
+				
+				@Override
+				public void addNavController(NavController contr) {
+				}
+			});
 		}
 	}
 
@@ -155,31 +240,34 @@ public class Launcher {
 		launcher.start();
 	}
 
-	private DrohnenController arDroneController;
-	private JFrame            jFrame;
-	private JButton           colorTrackButton;
-	private JLabel            monitor1;
+	private final DrohnenController arDroneController;
+	private JFrame jFrame;
+	private Monitor monitor1;
 
-	public JLabel getMonitor1() {
+	public Monitor getMonitor1() {
 		return monitor1;
 	}
 
-	public JLabel getMonitor2() {
+	public Monitor getMonitor2() {
 		return monitor2;
 	}
 
-	public JLabel getMonitor3() {
+	public Monitor getMonitor3() {
 		return monitor3;
 	}
 
-	public JLabel getMonitor0() {
+	public Monitor getMonitor0() {
 		return monitor0;
 	}
 
-	private       JLabel    monitor2;
-	private       JLabel    monitor3;
-	private       JLabel    monitor0;
+	private Monitor monitor2;
+	private Monitor monitor3;
+	private Monitor monitor0;
 	private final MainModel mainModel;
+
+	private JPanel content;
+
+	private JPanel centerPanel;
 
 	public Launcher() {
 
@@ -190,6 +278,9 @@ public class Launcher {
 		mainModel = new MainModel();
 
 		jFrame.setContentPane(getContent());
+		arDroneController = new WebCamController();
+
+		// arDroneController = new ARDroneController();
 
 		jFrame.addWindowListener(new WindowAdapter() {
 
@@ -199,19 +290,28 @@ public class Launcher {
 				getArDroneController().stop();
 				jFrame.dispose();
 
+				
+				Map<Thread, StackTraceElement[]> allStackTraces = Thread.getAllStackTraces();
+				for (Thread thread : allStackTraces.keySet()) {
+					if(thread.isAlive()){
+					}
+				}
 				System.exit(0);
 			}
 		});
 	}
 
-	private Container getContent() {
-		JPanel jPanel = new JPanel();
-		jPanel.setLayout(new BorderLayout());
+	private JPanel getContent() {
+		if (content == null) {
+			content = new JPanel();
+			content.setLayout(new BorderLayout());
+			content.setBorder(BorderFactory.createLoweredBevelBorder());
 
-		jPanel.add(new JScrollPane(getCenterPanel()), BorderLayout.CENTER);
-		jPanel.add(getSettingsPanel(), BorderLayout.WEST);
-		jPanel.add(getInformationPanel(), BorderLayout.SOUTH);
-		return jPanel;
+			content.add(getCenterPanel(), BorderLayout.CENTER);
+			content.add(getSettingsPanel(), BorderLayout.WEST);
+			content.add(getInformationPanel(), BorderLayout.SOUTH);
+		}
+		return content;
 	}
 
 	private Component getInformationPanel() {
@@ -222,70 +322,33 @@ public class Launcher {
 		JPanel jPanel = new JPanel();
 
 		JTabbedPane jTabbedPane = new JTabbedPane();
-		jTabbedPane.addTab("Steuerung Farbpanel", new JScrollPane(new ColorAdjustment(mainModel.getColorModel())));
-		jTabbedPane.addTab("Steuerung Edgepanel", new JScrollPane(new EdgeAdjustment(mainModel.getEdgeModel())));
+		jTabbedPane.addTab("Steuerung Farbpanel", new JScrollPane(
+				new ColorAdjustment(mainModel.getColorModel())));
+		jTabbedPane.addTab("Steuerung Edgepanel", new JScrollPane(
+				new EdgeAdjustment(mainModel.getEdgeModel())));
 
 		jPanel.add(jTabbedPane);
 		return jPanel;
 	}
 
 	private Component getCenterPanel() {
-		JPanel jPanel = new JPanel();
-		jPanel.setLayout(new GridLayout(2, 2));
+		if (centerPanel == null) {
 
-		monitor0 = new JLabel() {
-			@Override
-			public void setIcon(Icon icon) {
-				super.setIcon(icon);
-				if(icon == null) {
-					setText("Kein Bild auf Monitor 0");
-				} else {
-					setText("");
-				}
-			}
-		};
+			centerPanel = new JPanel();
+			centerPanel.setLayout(new GridLayout(2, 2));
 
-		monitor0.setBorder(MONITOR_BORDER);
-		monitor1 = new JLabel() {
-			@Override
-			public void setIcon(Icon icon) {
-				super.setIcon(icon);
-				if(icon == null) {
-					setText("Kein Bild auf Monitor 1");
-				} else {
-					setText("");
-				}
-			}
-		};
-		monitor2 = new JLabel() {
-			@Override
-			public void setIcon(Icon icon) {
-				super.setIcon(icon);
-				if(icon == null) {
-					setText("Kein Bild auf Monitor 2");
-				} else {
-					setText("");
-				}
-			}
-		};
-		monitor3 = new JLabel() {
-			@Override
-			public void setIcon(Icon icon) {
-				super.setIcon(icon);
-				if(icon == null) {
-					setText("Kein Bild auf Monitor 3");
-				} else {
-					setText("");
-				}
-			}
-		};
+			monitor0 = new Monitor(0);
+			monitor1 = new Monitor(1);
+			monitor2 = new Monitor(2);
+			monitor3 = new Monitor(3);
 
-		jPanel.add(monitor0);
-		jPanel.add(monitor1);
-		jPanel.add(monitor2);
-		jPanel.add(monitor3);
+			centerPanel.add(monitor0);
+			centerPanel.add(monitor1);
+			centerPanel.add(monitor2);
+			centerPanel.add(monitor3);
+		}
 
-		return jPanel;
+		return centerPanel;
 	}
 
 	private JMenuBar getMenu() {
@@ -296,7 +359,6 @@ public class Launcher {
 		ret.add(menuDrone);
 		menuDrone.add(new JMenuItem(actionConnect));
 		menuDrone.add(new JMenuItem(actionReset));
-		menuDrone.add(new JMenuItem(actionWebCam));
 
 		JMenu menuAnsicht = new JMenu();
 		menuAnsicht.setText("Ansicht");
@@ -305,6 +367,7 @@ public class Launcher {
 		menuAnsicht.add(new JMenuItem(actionShowLiveCam));
 		menuAnsicht.add(new JMenuItem(actionEdge));
 		menuAnsicht.add(new JMenuItem(actionShowColorTrack));
+		menuAnsicht.add(new JMenuItem(actionShowCircleTrack));
 		return ret;
 	}
 
@@ -313,6 +376,7 @@ public class Launcher {
 		actionReset.setEnabled(b);
 		actionEdge.setEnabled(b);
 		actionShowColorTrack.setEnabled(b);
+		actionShowCircleTrack.setEnabled(b);
 	}
 
 	private void start() {
@@ -323,4 +387,5 @@ public class Launcher {
 	public DrohnenController getArDroneController() {
 		return arDroneController;
 	}
+
 }
